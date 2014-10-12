62d62e6ee4ad9e87cdf51c1cc1d141eda0362541
Jean Brito <jeanfbrito@gmail.com>
2014-10-12 16:45:26 -0300

    add morris.js and raphael.js to create refuellings line chart

--- app/assets/javascripts/refuellings.js
+++ app/assets/javascripts/refuellings.js
@@ -1,3 +1,28 @@
+$(document).ready(function() {
+    lineChart();
+
+    $(window).resize(function() {
+        window.m.redraw();
+    });
+
+    $(document).on('page:change', function() {
+      window.m.redraw();
+    });
+});
+
+function lineChart() {
+    window.m = Morris.Line({
+    element: 'refuellings_chart',
+    data: $('#refuellings_chart').data('refuellings'),
+    xkey: 'date',
+    ykeys: ['average_consumption'],
+    labels: ['Consumo médio'],
+    xLabels: ["day"],
+    redraw: true,
+    postUnits: ' km/l'
+  });
+}
+
 $(document).ready(function(){
    $('.edit-refill-button').click(function() {
     var url;
