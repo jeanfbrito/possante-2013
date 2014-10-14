8794e57a116cf853846bd10270b4099b4b9965a0
Jean Brito <jeanfbrito@gmail.com>
2014-10-14 16:18:39 -0300

    remove jquery ui from assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require jquery-ui/datepicker
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
